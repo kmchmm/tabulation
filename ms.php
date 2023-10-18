bbc8930fa7337c832dae0304544d8ed1b609d82b
kmchmm <113099254+kmchmm@users.noreply.github.com>
2023-10-18 14:57:36 +0800

    first commit

---
+++ ms.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Tabulation Dashboard</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <nav class="justify-center align-center">
+                <div><img src="assets//images/ccs-logo.jpg" alt=""></div>
+                <ul class="justify-center">
+                    <li><a href="dashboard.php">Cultural Night</a></li>
+                    <li>Mr. & Miss</li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class="">
+            <div class="main-body">
+                <h1>Mr. & Miss Tabulation</h1>
+                <table id="applicants">
+                    <tr>
+                        <th>Contestant Name</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+
+                    <tr>
+                        <td class="applicant_name" style="display: none;">Sample</td>
+                        <td>Sample</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                </table>
+            </div>
+        </section>
+        <div class="logout-button">
+            <a href="">Log out</a>
+        </div>
+    </div>
+</body>
+</html>
